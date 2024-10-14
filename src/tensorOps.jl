dab942e8e9cf0b8b2db653437c32024911724f81
Tim Zuehlsdorff <52180151+tjz21@users.noreply.github.com>
2024-10-13 19:05:58 -0700

    Update tensorOps.jl

--- src/tensorOps.jl
+++ src/tensorOps.jl
@@ -4,7 +4,6 @@
 # simple implementation to replace it (that actually seems to be faster on CPUs 
 # as well)
 if GPU
-    using cuTENSOR
     import KrylovKit: exponentiate
     """
         exponentiate(A,t::Number,x,tol = 1e-12,maxiter = 50)
